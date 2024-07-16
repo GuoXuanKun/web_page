caa25782b482695255de2695460380959fe23ed0
GuoXuanKun <1925918787@qq.com>
2024-07-16 16:43:01 +0800

    SMS - 中文处理 跳过 富文本

--- SMS/src/main/java/com/java2403/filter/CharacterEncodingFilter.java
+++ SMS/src/main/java/com/java2403/filter/CharacterEncodingFilter.java
@@ -2,12 +2,26 @@ package com.java2403.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 @WebFilter(filterName = "CharacterEncodingFilter",value = "/*")
 public class CharacterEncodingFilter  implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+
+
+        HttpServletRequest req  = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp  = (HttpServletResponse) servletResponse;
+        // 获得 请求 路径
+        String uri =req.getRequestURI();
+        if(uri.contains("/ueditor")){
+            filterChain.doFilter(servletRequest,servletResponse);
+        }
+
+
 
 
         servletRequest.setCharacterEncoding("utf-8");
